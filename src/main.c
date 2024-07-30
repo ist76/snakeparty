28ad58aada56c6bf8daa0c3f7c049b2f87278359
ist76 <152007877+ist76@users.noreply.github.com>
2024-07-30 23:58:22 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -21,8 +21,6 @@
 #include "winio.h"
 #include "main.h"
 
-
-
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
     SaveData GSets = ReadSavegame();                             // Get previously saved data
